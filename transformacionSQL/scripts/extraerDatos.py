f10aa1ccbd1277d74db41ecd75c89b3f92df8422
ulisescorrales <ulisescorralesallen@gmail.com>
2025-04-22 15:45:47 -0300

    refactor: la lógica de extraer los datos de sqlite están en la carpeta transformacionSQL

--- transformacionSQL/scripts/extraerDatos.py
+++ transformacionSQL/scripts/extraerDatos.py
@@ -46,7 +46,7 @@ def consultar_y_dividir(conexion, consulta):
         res1=[] #Resultado thread1
         res2=[] #Resultado thread2
         t1=threading.Thread(target=getInserts,args=(resultadoConsulta,0,mitad,res1))
-        t2=threading.Thread(target=getInserts,args=(resultadoConsulta,mitad+1,len(resultadoConsulta)-1,res2))
+        t2=threading.Thread(target=getInserts,args=(resultadoConsulta,mitad+1,10000,res2))
         t1.start()
         t2.start()
 
@@ -81,7 +81,8 @@ def palabra_mas_larga_trans_list(conexion,consulta):
 
 if __name__ == "__main__":
     #Este script importa la base de datos desde el repositorio en WikDict, normaliza la tabla de traducciones a 4FN y exporta sentencias SQL compatible con la sintaxis de MySQL.
-    nombre_db = "./downloads/en-es.sqlite3"
+    print("Inicia construcción de sentencias.sql")
+    nombre_db = "./transformacionSQL/downloads/en-es.sqlite3"
     conexion = sqlite3.connect(nombre_db)
     consulta = "SELECT lexentry,written_rep,sense,trans_list FROM translation;"
     sentenciasInsert=consultar_y_dividir(conexion,consulta)
@@ -89,6 +90,7 @@ if __name__ == "__main__":
     #Consultar los tamaños de las columnas para crearlos en MySQL con VARCHAR(...)
     tamanioSenseConsulta="SELECT MAX(LENGTH(sense)) FROM translation;"
     tamanioLexentryConsulta="SELECT MAX(LENGTH(lexentry)) FROM translation;"
+    tamanioWrittenRepConsulta="SELECT MAX(LENGTH(written_rep)) FROM translation;"
 
     cursor1=conexion.cursor()
     tamanioSense=cursor1.execute(tamanioSenseConsulta).fetchall()[0][0]
@@ -96,11 +98,42 @@ if __name__ == "__main__":
     cursor2=conexion.cursor()
     tamanioLexentry=cursor2.execute(tamanioLexentryConsulta).fetchall()[0][0]
 
+    cursor3=conexion.cursor()
+    tamanioWrittenRep=cursor3.execute(tamanioWrittenRepConsulta).fetchall()[0][0]
+
     #Para trans_list como los datos no son atómicos se usa una función auxiliar
     tamanioTranslation=palabra_mas_larga_trans_list(conexion,"SELECT written_rep,trans_list FROM translation;")
     # print(tamanioSense)
     # print(tamanioLexentry)
     # print(tamanioTranslation)
-    with open("exports/sentencias.db","w",encoding="utf-8") as archivo:
-        archivo.write(sentenciasInsert)
+    #Generar la versión final del archivo .sql
+    sentencias=("START TRANSACTION;\n"
+    "CREATE TABLE translation_en_es (\n"
+    "id INT AUTO_INCREMENT PRIMARY KEY,\n"
+    "lexentry VARCHAR("+str(tamanioLexentry)+"),\n"
+    "word VARCHAR("+str(tamanioWrittenRep)+") NOT NULL,\n"
+    "sense VARCHAR("+str(tamanioSense)+"),\n"
+    "translate VARCHAR("+str(tamanioTranslation)+") NOT NULL,\n"
+    "INDEX word_search(word));\n")
+
+    sentencias+=sentenciasInsert
+    #Agregar el tipo de palabra
+    sentencias+="UPDATE translation_en_es SET lexentry='Adjective' WHERE lexentry LIKE '%Adjective%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Noun' WHERE lexentry LIKE '%Noun%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Preposition' WHERE lexentry LIKE '%Preposition%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Conjunction' WHERE lexentry LIKE '%Conjunction%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Interjection' WHERE lexentry LIKE '%Interjection%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Determiner' WHERE lexentry LIKE '%Determiner%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Suffix' WHERE lexentry LIKE '%Suffix%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Article' WHERE lexentry LIKE '%Article%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Numeral' WHERE lexentry LIKE '%Numeral%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Phrase' WHERE lexentry LIKE '%Phrase%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Prefix' WHERE lexentry LIKE '%Prefix%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Number' WHERE lexentry LIKE '%Number%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Adverb' WHERE lexentry LIKE '%Adverb%';\n"
+    sentencias+="UPDATE translation_en_es SET lexentry='Verb' WHERE lexentry LIKE '%Verb%' AND NOT lexentry='Adverb';\n" #Evitar que afecte a Adverb
+    sentencias+="COMMIT;"
+    with open("./transformacionSQL/output/sentencias.sql","w",encoding="utf-8") as archivo:
+        archivo.write(sentencias)
     conexion.close()
+    print("Finaliza la construccion de sentencias.sql")
